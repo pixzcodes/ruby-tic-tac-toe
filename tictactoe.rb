1cab37d987bb09990271967fb2f9fda550209c9a
Pixz <tinycactusking@gmail.com>
2022-10-19 00:04:25 -0400

    Add game board and empty main function

---
+++ tictactoe.rb
@@ -0,0 +1,14 @@
+game_board = [
+  "\n", 1, '|', 2, '|', 3, "\n",
+  '-', '-', '-', '-','-', "\n",
+  4, '|', 5, '|', 6, "\n",
+  '-', '-', '-', '-', '-', "\n",
+  7, '|', 8, '|', 9
+]
+
+def main
+  puts game_board.join(' ') # Display game board
+
+
+end
+main
